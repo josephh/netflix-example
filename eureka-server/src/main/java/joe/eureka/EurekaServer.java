a769b6c9ed9884c46743a183e0a585fe0fb8011b
josephh <josephh@rocketmail.com>
2017-03-21 15:24:57 +0000

    initial commit

---
+++ eureka-server/src/main/java/joe/eureka/EurekaServer.java
@@ -0,0 +1,19 @@
+package joe.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.system.ApplicationPidFileWriter;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+
+    public static void main(String[] args) {
+        SpringApplication eurekaServer = new SpringApplication(EurekaServer.class);
+        eurekaServer.addListeners(new ApplicationPidFileWriter("eureka-server.pid"));
+        eurekaServer.run();
+    }
+
+}
